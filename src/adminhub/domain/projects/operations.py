3b544a108584e022faed4d6fd633a39239ced1e5
Adam Parr <adamparr.uk@gmail.com>
2024-01-19 23:10:05 +0000

    add update_project operation

--- src/adminhub/domain/projects/operations.py
+++ src/adminhub/domain/projects/operations.py
@@ -4,3 +4,11 @@ from adminhub.data import models
 def create_project(name, description):
     project = models.Project.objects.create(name=name, description=description)
     return project
+
+
+def update_project(project: models.Project, name: str, description: str) -> models.Project:
+    project.name = name
+    project.description = description
+    project.save()
+
+    return project
