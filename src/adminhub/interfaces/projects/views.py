96fa731e6328f72abc68b37d565e06f8503a5bb4
Adam Parr <adamparr.uk@gmail.com>
2024-01-20 16:58:50 +0000

    add update project functionality, add navigation links to templates

--- src/adminhub/interfaces/projects/views.py
+++ src/adminhub/interfaces/projects/views.py
@@ -2,11 +2,11 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.views import generic
-# from django import forms
 from django import shortcuts
 from adminhub.domain.projects import queries
 from adminhub.domain.projects import operations
 from . import forms
+from django.urls import reverse
 
 
 class Projects(generic.ListView):
@@ -26,9 +26,7 @@ class Project(generic.DetailView):
     context_object_name = 'project_detail'
 
     def get_object(self) -> Any:
-        print(f"Fetching project with PK: {self.kwargs['pk']}")
         project = queries.get_project(pk=self.kwargs['pk'])
-        print(f"Retrieved project: {project.name}")
         return project
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
@@ -45,3 +43,31 @@ class CreateProject(generic.FormView):
             name=form.cleaned_data['name'], description=form.cleaned_data['description'])
 
         return shortcuts.redirect('project-detail', pk=project.pk)
+
+
+class UpdateProject(generic.FormView):
+    form_class = forms.ProjectForm
+    template_name = 'update-project.html'
+    context_object_name = 'project_detail'
+
+    def get_object(self) -> Any:
+        project = queries.get_project(pk=self.kwargs['pk'])
+        return project
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context[self.context_object_name] = self.get_object()
+        return context
+
+    def get_initial(self) -> dict[str, Any]:
+        initial = super().get_initial()
+        project = self.get_object()
+        initial['name'] = project.name
+        initial['description'] = project.description
+        return initial
+
+    def form_valid(self, form: Any) -> HttpResponse:
+        project = operations.update_project(pk=self.kwargs['pk'],
+                                            name=form.cleaned_data['name'], description=form.cleaned_data['description'])
+
+        return shortcuts.redirect(reverse('project-detail', kwargs={'pk': project.pk}))
