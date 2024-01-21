32c1c9831c2150dc5fd2a9b42c43a2d50b23e154
Adam Parr <adamparr.uk@gmail.com>
2024-01-21 19:36:26 +0000

    adjust and clean up forms and views

--- src/adminhub/interfaces/projects/forms.py
+++ src/adminhub/interfaces/projects/forms.py
@@ -1,24 +1,20 @@
-from collections.abc import Mapping
 from django import forms
-from django.forms.utils import ErrorList
 from adminhub.data import models
 
 
-class CreateProjectForm(forms.Form):
+class CreateProject(forms.Form):
     project_fields = forms.fields_for_model(models.Project)
 
     name = project_fields['name']
     description = project_fields['description']
 
 
-class ProjectForm(forms.Form):
-    def __init__(self, project: models.Project, *args, **kwargs) -> None:
-        self.project = project
+class UpdateProject(forms.Form):
+    name = forms.CharField(max_length=30)
+    description = forms.CharField(max_length=120)
+
+    def __init__(self, project, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.fields['name'].initial = project.name
-        self.fields['description'].initial = project.description
-
-    project_fields = forms.fields_for_model(models.Project)
-
-    name = project_fields['name']
-    description = project_fields['description']
+        if project:
+            self.fields['name'].initial = project.name
+            self.fields['description'].initial = project.description
--- src/adminhub/interfaces/projects/views.py
+++ src/adminhub/interfaces/projects/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.http import HttpRequest, HttpResponse
+from django import http
 from django.views import generic
 from django import shortcuts
 from adminhub.domain.projects import queries
@@ -36,10 +36,10 @@ class Project(generic.DetailView):
 
 
 class CreateProject(generic.FormView):
-    form_class = forms.CreateProjectForm
+    form_class = forms.CreateProject
     template_name = 'create-project.html'
 
-    def form_valid(self, form: Any) -> HttpResponse:
+    def form_valid(self, form: Any) -> http.HttpResponse:
         project = operations.create_project(
             name=form.cleaned_data['name'], description=form.cleaned_data['description'])
 
@@ -47,20 +47,21 @@ class CreateProject(generic.FormView):
 
 
 class UpdateProject(generic.FormView):
-    form_class = forms.ProjectForm
+    form_class = forms.UpdateProject
     template_name = 'update-project.html'
     context_object_name = 'project_detail'
 
-    def setup(self, request: HttpRequest, *args: Any, **kwargs: Any) -> None:
+    def setup(self, request: http.HttpRequest, *args: Any, **kwargs: Any) -> None:
         self.project = shortcuts.get_object_or_404(
             models.Project, pk=kwargs.get('pk'))
         return super().setup(request, *args, **kwargs)
 
-    def form_valid(self, form: Any) -> HttpResponse:
+    def form_valid(self, form: Any) -> http.HttpResponse:
         project = operations.update_project(self.project,
                                             name=form.cleaned_data['name'], description=form.cleaned_data['description'])
 
-        return shortcuts.redirect(reverse('project-detail', kwargs={'pk': project.pk}))
+        success_url = self.get_success_url(project)
+        return shortcuts.redirect(success_url)
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
@@ -71,3 +72,6 @@ class UpdateProject(generic.FormView):
         kwargs = super().get_form_kwargs()
         kwargs['project'] = self.project
         return kwargs
+
+    def get_success_url(self, project: models.Project) -> str:
+        return reverse('project-detail', kwargs={'pk': project.pk})
